63ab776423b785047e99830f3819bc5e18368d64
Tenzin Dorjee <tenzindorjee95@yahoo.com>
2023-12-14 01:49:31 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let  i = 0; i<array.length; i++){
+    for (let j=i+1; j<array.length; j++){
+      if(array[i]+array[j]===target){
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /* 
